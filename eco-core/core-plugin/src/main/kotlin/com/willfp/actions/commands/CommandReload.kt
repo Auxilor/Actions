48e98470a087ad538756f8990fa9bfafc0bcacce
Exanthiax <107284021+Exanthiax@users.noreply.github.com>
2026-03-19 13:09:50 +0000

    Update reload message to include action count

--- eco-core/core-plugin/src/main/kotlin/com/willfp/actions/commands/CommandReload.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/actions/commands/CommandReload.kt
@@ -1,5 +1,6 @@
 package com.willfp.actions.commands
 
+import com.willfp.actions.actions.Actions
 import com.willfp.actions.plugin
 import com.willfp.eco.core.command.impl.Subcommand
 import com.willfp.eco.util.StringUtils
@@ -16,6 +17,7 @@ object CommandReload : Subcommand(
         sender.sendMessage(
             plugin.langYml.getMessage("reloaded", StringUtils.FormatOption.WITHOUT_PLACEHOLDERS)
                 .replace("%time%", plugin.reloadWithTime().toNiceString())
+                .replace("%count%", Actions.values().size.toString())
         )
     }
 }
