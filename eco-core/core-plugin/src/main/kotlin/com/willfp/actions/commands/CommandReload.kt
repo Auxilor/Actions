bd30ee57b5d80d0e43840b9e0fe60b03b39a2ce5
NicoNeko <nicuch100@gmail.com>
2026-04-20 22:23:24 +0300

    Add folia support

--- eco-core/core-plugin/src/main/kotlin/com/willfp/actions/commands/CommandReload.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/actions/commands/CommandReload.kt
@@ -2,6 +2,7 @@ package com.willfp.actions.commands
 
 import com.willfp.actions.actions.Actions
 import com.willfp.actions.plugin
+import com.willfp.eco.core.Prerequisite
 import com.willfp.eco.core.command.impl.Subcommand
 import com.willfp.eco.util.StringUtils
 import com.willfp.eco.util.toNiceString
@@ -14,10 +15,16 @@ object CommandReload : Subcommand(
     false
 ) {
     override fun onExecute(sender: CommandSender, args: List<String>) {
-        sender.sendMessage(
-            plugin.langYml.getMessage("reloaded", StringUtils.FormatOption.WITHOUT_PLACEHOLDERS)
-                .replace("%time%", plugin.reloadWithTime().toNiceString())
-                .replace("%count%", Actions.values().size.toString())
-        )
+        val runnable: Runnable = {
+            sender.sendMessage(
+                plugin.langYml.getMessage("reloaded", StringUtils.FormatOption.WITHOUT_PLACEHOLDERS)
+                    .replace("%time%", plugin.reloadWithTime().toNiceString())
+                    .replace("%count%", Actions.values().size.toString())
+            )
+        }
+        if (Prerequisite.HAS_FOLIA.isMet)
+            plugin.scheduler.runTask(runnable) // run on global thread
+        else
+            runnable.run()
     }
 }
