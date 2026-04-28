c4c98246d0633958f3ab4171270ff1e6513b4e04
Exanthiax <107284021+Exanthiax@users.noreply.github.com>
2026-04-28 23:23:11 +0100

    Revert Folia and Bump Eco to 7.4.2 (#11)
    * Revert "Add folia support (#6)"
    
    This reverts commit 037131817045977e2b7dee06944cf8cab7067b7e.
    
    * chore: bump eco to 7.4.2

--- eco-core/core-plugin/src/main/kotlin/com/willfp/actions/commands/CommandReload.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/actions/commands/CommandReload.kt
@@ -2,7 +2,6 @@ package com.willfp.actions.commands
 
 import com.willfp.actions.actions.Actions
 import com.willfp.actions.plugin
-import com.willfp.eco.core.Prerequisite
 import com.willfp.eco.core.command.impl.Subcommand
 import com.willfp.eco.util.StringUtils
 import com.willfp.eco.util.toNiceString
@@ -15,16 +14,10 @@ object CommandReload : Subcommand(
     false
 ) {
     override fun onExecute(sender: CommandSender, args: List<String>) {
-        val runnable: Runnable = {
-            sender.sendMessage(
-                plugin.langYml.getMessage("reloaded", StringUtils.FormatOption.WITHOUT_PLACEHOLDERS)
-                    .replace("%time%", plugin.reloadWithTime().toNiceString())
-                    .replace("%count%", Actions.values().size.toString())
-            )
-        }
-        if (Prerequisite.HAS_FOLIA.isMet)
-            plugin.scheduler.runTask(runnable) // run on global thread
-        else
-            runnable.run()
+        sender.sendMessage(
+            plugin.langYml.getMessage("reloaded", StringUtils.FormatOption.WITHOUT_PLACEHOLDERS)
+                .replace("%time%", plugin.reloadWithTime().toNiceString())
+                .replace("%count%", Actions.values().size.toString())
+        )
     }
 }
