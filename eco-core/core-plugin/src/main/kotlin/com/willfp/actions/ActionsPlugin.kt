27b356ce83682886b73054d4384017d97cb0e1ba
Callum Johnson <74352969+CallumJohnson@users.noreply.github.com>
2026-05-16 15:36:50 +0100

    feat: BStats Metrics

--- eco-core/core-plugin/src/main/kotlin/com/willfp/actions/ActionsPlugin.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/actions/ActionsPlugin.kt
@@ -2,6 +2,7 @@ package com.willfp.actions
 
 import com.willfp.actions.actions.Actions
 import com.willfp.actions.commands.CommandActions
+import com.willfp.eco.core.bstats.EcoMetricsChart
 import com.willfp.eco.core.command.impl.PluginCommand
 import com.willfp.libreforge.SimpleProvidedHolder
 import com.willfp.libreforge.loader.LibreforgePlugin
@@ -35,5 +36,25 @@ class ActionsPlugin : LibreforgePlugin() {
             CommandActions
         )
     }
+
+    override fun getCustomCharts() = listOf(
+        EcoMetricsChart.SingleLine("total_actions") { Actions.values().size },
+        EcoMetricsChart.SingleLine("enabled_actions") { Actions.values().count { it.enabled } },
+        EcoMetricsChart.SimplePie("cooldown_message_enabled") {
+            if (configYml.getBool("cooldown.message-enabled")) "enabled" else "disabled"
+        },
+        EcoMetricsChart.SimplePie("cooldown_actionbar") {
+            if (configYml.getBool("cooldown.in-actionbar")) "enabled" else "disabled"
+        },
+        EcoMetricsChart.SimplePie("cooldown_sound") {
+            if (configYml.getBool("cooldown.sound")) "enabled" else "disabled"
+        },
+        EcoMetricsChart.SimplePie("telekinesis_on_players") {
+            if (configYml.getBool("effects.telekinesis.on-players")) "enabled" else "disabled"
+        },
+        EcoMetricsChart.SimplePie("use_setblock_break") {
+            if (configYml.getBool("effects.use-setblock-break")) "enabled" else "disabled"
+        }
+    )
 }
 
